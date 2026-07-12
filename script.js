bffc1f61885fb372ba83a787879d06f8cda9fe8c
Debajit Saha <debajitsahacse@gmail.com>
2026-07-12 13:53:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,57 @@
-const text = "B. Tech CSE Student | Software Developer | Problem Solver";
-let i = 0;
+const texts = [
+    "B.Tech CSE Student",
+    "Software Developer",
+    "Flask Backend Developer",
+    "Problem Solver"
+];
 
-function typeEffect() {
-    if (i < text.length) {
-        document.getElementById("typing").innerHTML += text.charAt(i);
-        i++;
-        setTimeout(typeEffect, 60);
+let index = 0;
+let charIndex = 0;
+let currentText = "";
+
+function typeEffect(){
+
+    if(charIndex < texts[index].length){
+
+        document.getElementById("typing").innerHTML += 
+        texts[index].charAt(charIndex);
+
+        charIndex++;
+
+        setTimeout(typeEffect,100);
+
+    }
+    else{
+
+        setTimeout(eraseEffect,1500);
+
     }
 }
+
+
+function eraseEffect(){
+
+    if(charIndex > 0){
+
+        document.getElementById("typing").innerHTML =
+        texts[index].substring(0,charIndex-1);
+
+        charIndex--;
+
+        setTimeout(eraseEffect,50);
+
+    }
+    else{
+
+        index++;
+
+        if(index >= texts.length)
+            index=0;
+
+        setTimeout(typeEffect,500);
+    }
+
+}
+
 
 typeEffect();
